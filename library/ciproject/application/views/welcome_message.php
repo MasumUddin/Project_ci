624764946adc906f3b249c6b3b87737e58c1187e
Your Name <you@example.com>
2019-01-16 16:45:13 +0600

    php project

---
+++ library/ciproject/application/views/welcome_message.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to employee training record</title>
+
+	
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to employee training record</h1>
+	</div>
+
+	
+
+
+
+</body>
+</html>
